73ad3067bc35f2c84fcb8f7a978d85e4a2ae0e2b
Ksnarf <44852455+Ksnarf@users.noreply.github.com>
2023-09-05 07:33:42 -0700

    add css and js

---
+++ script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(function () {
+    document.getElementById('splashScreen').classList.add('hidden');
+    document.getElementById('console').classList.remove('hidden');
+  }, 3000);
+
+  const input = document.getElementById('input');
+  const output = document.getElementById('output');
+
+  input.addEventListener('keydown', function (e) {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      const command = input.value;
+      input.value = '';
+
+      if (command === 'help') {
+        output.innerHTML += 'Available commands: help<br>';
+      } else {
+        output.innerHTML += 'Unknown command<br>';
+      }
+    }
+  });
+});
