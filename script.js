506264661b10e8f90c894423d40b6b69faa25b23
Ksnarf <44852455+Ksnarf@users.noreply.github.com>
2023-10-29 09:59:46 -0700

    add ui focus
    Once welcome message ends, should auto focus to the text prompt

--- script.js
+++ script.js
@@ -2,19 +2,15 @@ document.addEventListener("DOMContentLoaded", function () {
   let helpTimeout; // Declare this variable at the top of your script
 
   setTimeout(function () {
-    document.getElementById('splashScreen').classList.add('hidden');
-    document.getElementById('console').classList.remove('hidden');
-    document.getElementById('input').focus();
-    
-    //Welcome Message
-    document.getElementByID('output').innerHTML += "Welcome to ksnarf.dev OS. This interface will allow you to search existing services, execute programs to get more data on each service and see what we are currently working on. We hope this simplified interface helps show you our approach to what we do; while we may not be the flashiest, what we build works <br> Here are some of the commands you can use:<br>fivem= to see a list of active and in-progress fivem modules<br>";
-    
-    
-    // Set a new timeout for the help message
-    helpTimeout = setTimeout(function() {
-      document.getElementById('output').innerHTML += "Type 'help' to get started or enter the name of the program you want to run.<br>";
-    }, 3000);
-  }, 3000);
+  document.getElementById('splashScreen').classList.add('hidden');
+  document.getElementById('console').classList.remove('hidden');
+  
+  // Add the welcome message to the 'output' div
+  document.getElementById('output').innerHTML += "Welcome to ksnarf.dev OS. This interface will allow you to search, execute and download data related to ksnarf.dev services and builds. We hope this simplified interface helps show you our approach to services; while we may not be the flashiest, what we build works (or at least so we hope).<br>Here are some of the commands you can use:<br>fivem = to see a list of active and in-progress fivem modules<br>";
+  
+  // Set the focus to the input field after the welcome message
+  document.getElementById('input').focus();
+}, 3000);
 
   const input = document.getElementById('input');
   const output = document.getElementById('output');
